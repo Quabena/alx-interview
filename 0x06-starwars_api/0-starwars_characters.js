4c4a96615b5c5a8ee8cf438b363d777e92b041a1
Evans <ivanz.corbie@gmail.com>
2025-05-28 01:52:46 +0000

    0-starwars_characters.js

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.error('Usage: ./0-starwars_characters.js <movie_id>');
+  process.exit(1);
+}
+
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const film = JSON.parse(body);
+  const characters = film.characters;
+
+  function fetchCharacter(index) {
+    if (index >= characters.length) return;
+
+    request(characters[index], (err2, res2, body2) => {
+      if (err2) {
+        console.error(err2);
+        return;
+      }
+
+      const character = JSON.parse(body2);
+      console.log(character.name);
+      fetchCharacter(index + 1);
+    });
+  }
+
+  fetchCharacter(0);
+});
